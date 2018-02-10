700fa9ba46732beb23fe048c0d360be6eac60570
Sandeep Joshi <joshi.sandeep.d@gmail.com>
2018-02-10 22:15:12 +0530

    Corrected charts layout.

--- src/components/containers/charts/VolumeChart.js
+++ src/components/containers/charts/VolumeChart.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactHighcharts from 'react-highcharts';
 
 const config = {
-    chart: {
+      chart: {
           type: 'bubble',
           plotBorderWidth: 1,
+          height: (9 / 21 * 100) + '%'
       },
       legend: {
           enabled: false
@@ -70,14 +71,15 @@ const config = {
       },
 
       series: [{
+          color : 'rgba(255, 255, 255, 0.7)',
           data: [
               { x: 5, y: 5, z: 18000, name: 'Krakan'},
               { x: 11, y: 11, z: 11000, name: 'Bitfinex'},
-              { x: 8, y: 8, z: 8000, name: 'Binance'},
+              { x: 8, y: 5, z: 8000, name: 'Binance'},
               { x: 21, y: 21, z: 21000, name: 'CEX.IO'},
               { x: 13, y: 13, z:13000, name: 'Bittrex'},
-              { x: 7, y: 7, z: 7100, name: 'Poloniex'},
-              { x: 9, y: 9, z: 9000, name: 'HitBTC'},
+              { x: 8, y: 12, z: 7100, name: 'Poloniex'},
+              { x: 9, y: 18, z: 9000, name: 'HitBTC'},
               { x: 18, y: 18, z: 22000, name: 'GDAX'},
           ]
       }]
@@ -86,7 +88,7 @@ const config = {
 export default class VolumeChart extends React.Component {
   render() {
     return (
-        <div className="chart-bottom"  >
+        <div className="chart-bottom chart-container"  >
             <ReactHighcharts config={config} ref="chart" />
         </div>
     );
